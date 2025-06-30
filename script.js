0b1f139a6c506fa2bcf20d3cba09e98488b26dbd
sivaramakrishnaveeranki <sivaramakrishnaveeranki59@gmail.com>
2025-06-30 10:44:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const navbar = document.getElementById("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
